49f622a5e7bc5a5d44bbfb0968cc9f497f9a3cbf
hoangk66husterIT <tranhuyhoangk14a1@gmail.com>
2023-12-14 15:53:47 +0700

    Lab 05

--- GUIProject/src/hust/soict/hedspi/javafx/Painter.java
+++ GUIProject/src/hust/soict/hedspi/javafx/Painter.java
@@ -1,9 +1,24 @@
 package hust.soict.hedspi.javafx;
 
-public class Painter {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public Painter() {
-		// TODO Auto-generated constructor stub
+public class Painter extends Application{
+
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/hust/soict/hedspi/javafx/Painter.fxml"));
+		Scene scene = new Scene(root);
+		stage.setTitle("Painter");
+		stage.setScene(scene);
+		stage.show();
 	}
-
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
---
+++ GUIProject/src/hust/soict/hedspi/javafx/PainterController.java
@@ -0,0 +1,46 @@
+package hust.soict.hedspi.javafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class PainterController {
+
+    @FXML
+    private Pane drawingAreaPane;
+    
+    private boolean pen;
+
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+        drawingAreaPane.getChildren().clear();
+    }
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event) {
+    	Color color = null;
+    	if(pen) {
+    		color = Color.BLACK;
+    	}
+    	else {
+    		color = Color.WHITE;
+    	}
+        Circle newCircle = new Circle(event.getX(), event.getY(), 4);
+        newCircle.setFill(color);
+        drawingAreaPane.getChildren().add(newCircle);
+    }
+    
+
+    @FXML
+    void setPen(ActionEvent event) {
+    	pen = true;
+    }
+
+    @FXML
+    void setEraser(ActionEvent event) {
+    	pen = false;
+    }
+}
